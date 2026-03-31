15096d4aab76c93e89e104ee95752a1525f23759
Nicolas Silveira Teixeira <nicolassilveirateixeira@gmail.com>
2026-03-31 09:28:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ def post_jogo():
     arquivo.add_jogo(nome, desc)
     return arquivo.teste()
 
+@app.route("/jogo/rmv", methods=["DELETE"])
+def post_jogo():
+    nome = request.json["nome"]
+    desc = request.json["desc"]
+    arquivo.add_jogo(nome, desc)
+    return arquivo.teste()
 app.run()
 # curl -X POST http://127.0.0.1:5000/jogo/add -H "Content-Type: application/json" -d "{\"nome\":\"Mario\",\"desc\":\"Odissey\"}"
--- arquivo.py
+++ arquivo.py
@@ -4,6 +4,16 @@ from classes import Usuario
 usuario1 = Usuario("Dudu_games", "senhat0p")
 
 
+usuario1.adicionar_jogo("nome","desc")
+usuario1.adicionar_jogo("nome2","desc2")
+usuario1.adicionar_jogo("nome3","desc3")
+
+print(usuario1.listar_jogos())
+
+usuario1.remover_jogo()
+
+print(usuario1.listar_jogos())
+
 def add_jogo(nome, desc):
 
     usuario1.adicionar_jogo(nome,desc)
@@ -11,3 +21,6 @@ def add_jogo(nome, desc):
 def teste():
     
     return usuario1.listar_jogos()
+
+def rmv_jogo():
+    return usuario1.remover_jogo()
